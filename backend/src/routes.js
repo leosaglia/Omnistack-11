9825bb9e9b091df41372fc1a5fdb3060688c8167
Leo Saglia <leosaglia@hotmail.com>
2020-03-24 21:37:11 -0300

    Backend da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import app from './index';
+import OngController from './controllers/OngController';
+import IncidentController from './controllers/IncidentController';
+import ProfileController from './controllers/ProfileController';
+import SessionController from './controllers/SessionController';
+
+const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+export default routes;
